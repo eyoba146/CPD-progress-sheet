4f56d3bb7244d2c29699e82eac1dab2ca615a946
eyoba146 <132304110+eyoba146@users.noreply.github.com>
2025-10-29 12:46:21 -0700

    Add twoSum method in Solution class
    Implement twoSum method to find indices of numbers that add up to target.

---
+++ two_sumII.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        pointer1 = 0
+        pointer2 = len(numbers)-1
+        for _ in range(len(numbers)):
+            if numbers[pointer1] + numbers[pointer2] == target:
+                return [pointer1+1,pointer2+1]
+            elif numbers[pointer1] + numbers[pointer2] > target:
+                pointer2-=1
+            elif numbers[pointer1] + numbers[pointer2] < target:
+                pointer1+=1
