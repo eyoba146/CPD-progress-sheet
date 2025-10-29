92583edf15c3fdc13e8571554c1c8c11889b6234
eyoba146 <132304110+eyoba146@users.noreply.github.com>
2025-10-29 14:40:06 -0700

    Update Valid Palindrome.py

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -5,12 +5,15 @@ class Solution:
         for i in range(len(s)):
             if s[i].isalpha() or s[i].isdigit():
                 letters.append(s[i].lower())
+                
         if len(letters) == 0:
             return True   
+            
         r = len(letters) -1
         while r > l:
             if letters[l] != letters[r]:
                 return False
             r-=1
             l+=1
+            
         return True
