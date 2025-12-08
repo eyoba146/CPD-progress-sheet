7b67f562072b57a2d80963cd3265046ac8ebac4c
eyoba146 <132304110+eyoba146@users.noreply.github.com>
2025-12-08 13:28:35 -0300

    Add numRescueBoats function to calculate boat count
    Implement a solution to count the number of boats needed to rescue people based on their weights and a weight limit.

---
+++ boats_to_save_people.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left = 0
+        right = len(people)-1
+        boat_count = 0
+        while left <= right:
+            if people[left] >= limit:
+                boat_count+=1
+                left+=1
+            if people[right] >= limit:
+                boat_count+=1
+                right-=1
+            if left == right:
+                boat_count+=1
+                break
+            if people[left]+people[right] > limit:
+                if limit-people[right] <= people[left]:
+                    boat_count+=1
+                    right-=1
+                else:
+                    boat_count+=2
+                    left+=1
+                    right-=1
+            if people[left]+people[right] <= limit:
+                boat_count+=1
+                left+=1
+                right-=1
+        return boat_count
+
+        
