c6fde13ef66ac4ce01b7d5ef81c3ec859aee5771
eyoba146 <132304110+eyoba146@users.noreply.github.com>
2026-02-07 14:13:47 -0300

    Implement subarray sum equals k function

--- subarray_sum_equals_k.py
+++ subarray_sum_equals_k.py
@@ -1,13 +1,12 @@
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
-        current_sum = nums[0]
-        for i in range(1, len(nums)):
-            if current_sum + nums[i] > nums[i]:
-                current_sum = current_sum + nums[i]
-            else:
-                current_sum = nums[i]
-            if current_sum > max_sum:
-                max_sum = current_sum
-        return max_sum
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        prefix_sum = 0
+        prefix_counts = {0: 1}
+        for num in nums:
+            prefix_sum += num
+            if (prefix_sum - k) in prefix_counts:
+                count += prefix_counts[prefix_sum - k]
+            prefix_counts[prefix_sum] = prefix_counts.get(prefix_sum, 0) + 1
 
+        return count
