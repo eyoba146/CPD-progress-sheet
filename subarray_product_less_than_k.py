2b54bd5f7eab7f6a843087f297750131b868624e
eyoba146 <132304110+eyoba146@users.noreply.github.com>
2026-02-07 13:32:20 -0300

    Implement method to count subarrays with product < k

---
+++ subarray_product_less_than_k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+        product = 1
+        left = 0
+        result = 0
+        for right in range(len(nums)):
+            product *= nums[right]
+            while product >= k and left <= right:
+                product //=nums[left]
+                left += 1
+            result += right - left + 1
+        return result
