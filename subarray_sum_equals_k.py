153d4037e13d437cd05e25f89fc41edf3eb8714a
eyoba146 <132304110+eyoba146@users.noreply.github.com>
2026-02-07 14:10:12 -0300

    Add maxSubArray method to Solution class

---
+++ subarray_sum_equals_k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        current_sum = nums[0]
+        for i in range(1, len(nums)):
+            if current_sum + nums[i] > nums[i]:
+                current_sum = current_sum + nums[i]
+            else:
+                current_sum = nums[i]
+            if current_sum > max_sum:
+                max_sum = current_sum
+        return max_sum
+
